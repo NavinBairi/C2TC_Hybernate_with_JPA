2662580e9f3cb8bf2f3caa2eb376898472fd3b11
Navin Bairi <Navin Bairi@Navin>
2022-07-14 17:14:12 +0530

    Single_table

---
+++ JPA_ST/src/com/tns/entity/Manager.java
@@ -0,0 +1,24 @@
+package com.tns.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="mgr_store")
+public class Manager extends Employee {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -749410526826476802L;
+	
+	private String departmentName;
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+}
+	
